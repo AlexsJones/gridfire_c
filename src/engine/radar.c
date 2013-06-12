d2a7397b859669e92b153122cb1f3cbae35e5ee0
AlexsJones <alexsimonjones@gmail.com>
2013-06-12 07:34:27 +0100

    about to try another idea and use a view for radar

--- src/engine/radar.c
+++ src/engine/radar.c
@@ -17,17 +17,30 @@
  */
 #include <stdlib.h>
 #include "radar.h"
-
+#include <jnxc_headers/jnxhash.h>
+#include "game_object.h"
+extern jnx_hashmap *config;
 sfRectangleShape *radar_background;
+
+
+sfVector2f radar_position_translation(game_object *obj)
+{
+	//translate the object's ingame position to something in the radar
+}
 
 void radar_setup(void)
 {
+	/*-----------------------------------------------------------------------------
+	 *  The radar will translate based on mapsize
+	 *-----------------------------------------------------------------------------*/
+
 	radar_background = sfRectangleShape_create();
 	sfVector2f radar_size;
 	radar_size.x = 250;
 	radar_size.y = 250;
-	sfRectangleShape_setOutlineColor(radar_background,sfColor_fromRGB(255,245,235));
-	sfRectangleShape_setFillColor(radar_background,sfColor_fromRGB(255,235,235));
+	sfRectangleShape_setOutlineThickness(radar_background,2);
+	sfRectangleShape_setOutlineColor(radar_background,sfColor_fromRGB(50,50,50));
+	sfRectangleShape_setFillColor(radar_background,sfColor_fromRGB(1,1,1));
 	sfRectangleShape_setSize(radar_background,radar_size);
 }
 void radar_update(sfView *view,sfRenderWindow *main_window, jnx_list *list)
